f9156749e1be3eb96c883bdae71afac55c285a8e
your.user.name <harmanmalht777@gmail.com>
2023-09-10 17:58:27 +0530

    Minor warning fix

--- inotes/lib/main.dart
+++ inotes/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:inotes/firebase_options.dart';
-import 'package:inotes/views/login_view.dart';
+// import 'package:inotes/views/login_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
